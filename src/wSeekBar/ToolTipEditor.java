a0e298d573ba4df8069b93e35c9d305e4fb3975c
Michael Wasylyk <mikewas261@gmail.com>
2013-09-05 21:01:41 -0400

    EGIT pull cleanup

--- src/wSeekBar/ToolTipEditor.java
+++ src/wSeekBar/ToolTipEditor.java
@@ -4,4 +4,5 @@ package wSeekBar;
 public class ToolTipEditor {
 	//TODO show ToolTipEditor with time mark and name along with delete button
 	//TODO popup on mouseover of bookmark
+	//EGIT TEST REMOVE
 }
